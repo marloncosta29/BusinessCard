25e4a8640cc48247b5bca55a5fa6f96f04210357
marlon <marlon.29@outlook.com>
2021-09-26 16:50:01 -0300

    Finalizando projeto para MVP

--- app/src/main/java/com/marlon/businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/marlon/businesscard/ui/MainActivity.kt
@@ -3,14 +3,25 @@ package com.marlon.businesscard.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.marlon.businesscard.App
 import com.marlon.businesscard.R
 import com.marlon.businesscard.databinding.ActivityMainBinding
+import com.marlon.businesscard.util.Image
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+    private val adapter by lazy {
+        BusinessCardAdapter()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.rvCards.adapter = adapter
+        getAllBusinessCard()
         inicializeListeners()
     }
 
@@ -19,8 +30,16 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java )
             startActivity(intent)
         }
+        adapter.listenerShare = { card ->
+            Image.share(this@MainActivity, card)
+        }
 
     }
 
+    private fun getAllBusinessCard(){
+        mainViewModel.getAll().observe(this, { businessCard ->
+            adapter.submitList(businessCard)
+        })
+    }
 
 }
